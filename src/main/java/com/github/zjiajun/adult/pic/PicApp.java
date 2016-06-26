26983bc5e8c167285a16907a39c646458b7a6629
zhujiajun <zhujiajun.me@gmail.com>
2016-06-26 20:36:12 +0800

    refactoring pageInfo

--- src/main/java/com/github/zjiajun/adult/pic/PicApp.java
+++ src/main/java/com/github/zjiajun/adult/pic/PicApp.java
@@ -7,6 +7,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.github.zjiajun.adult.tool.AdultConfig.*;
 import static com.github.zjiajun.adult.tool.AdultTool.*;
@@ -19,18 +21,32 @@ public class PicApp {
 
 
     public void handlerRosixz() {
+        Map<Integer, String> pageInfo = getRosixzPageInfo();
+        for (String pageUrls: pageInfo.values()) {
+
+
+        }
+
     }
 
+
+
     /**
-     * 获取rosixz的总页数
-     * @return 总页数
+     * 获取rosixz的分页信息
+     * @return Map[page->pageUrl]
      */
-    private int getRosixzPageTotal() {
+    private Map<Integer,String> getRosixzPageInfo() {
         try {
-            Document indexDoc = Jsoup.connect(rosixzUrl()).userAgent(randomUa()).get();
-            Elements pageElements = indexDoc.select(".page-navigator");
-            Element element = pageElements.get(pageElements.size() - 1);
-            return 1;
+            Document indexDoc = Jsoup.connect(rosixzUrl()).userAgent(randomUa()).timeout(5000).get();
+            Elements pageElements = indexDoc.select(".page-navigator li a");
+            Element totalElement = pageElements.get(pageElements.size() - 2);//倒数第2个是总页数
+            String pageUrl = totalElement.absUrl("href");
+            String totalSize = totalElement.html();
+            Map<Integer,String> pageInfo = new HashMap<>();
+            for (int page = Integer.parseInt(totalSize); page > 0; page--) {
+                pageInfo.put(page,pageUrl.replace(totalSize,String.valueOf(page)));
+            }
+            return pageInfo;
         } catch (IOException e) {
             throw new AdultException("Handler rosixz exception : " + e.getMessage());
         }
---
+++ src/test/java/com/github/zjiajun/adult/PicAppTest.java
@@ -0,0 +1,45 @@
+package com.github.zjiajun.adult;
+
+import com.github.zjiajun.adult.pic.PicApp;
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * Created by zhujiajun
+ * 16/6/26 12:59
+ */
+public class PicAppTest {
+
+    private static PicApp picApp;
+    private static final String GET_ROSIXZ_PAGE_INFO = "getRosixzPageInfo";
+
+    @BeforeClass
+    public static void setPicApp() {
+        picApp = new PicApp();
+    }
+
+    @Test
+    public void shouldEqualsPageCount() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method getRosixzPageInfoMethod = PicApp.class.getDeclaredMethod(GET_ROSIXZ_PAGE_INFO);
+        getRosixzPageInfoMethod.setAccessible(true);
+        Object value = getRosixzPageInfoMethod.invoke(picApp);
+        Map<Integer, String> pageInfo = (Map<Integer, String>) value;
+        assertEquals(pageInfo.size(),22);
+    }
+
+    @Test
+    public void shouldEqualsPageInfo() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method getRosixzPageInfoMethod = PicApp.class.getDeclaredMethod(GET_ROSIXZ_PAGE_INFO);
+        getRosixzPageInfoMethod.setAccessible(true);
+        Object value = getRosixzPageInfoMethod.invoke(picApp);
+        Map<Integer, String> pageInfo = (Map<Integer, String>) value;
+        assertEquals(pageInfo.get(1),"http://www.rosixz.com/page-1.html");
+
+    }
+}
