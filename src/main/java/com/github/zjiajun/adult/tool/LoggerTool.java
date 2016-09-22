e9b2f12356a20c8b4582fb8d7c6fbd52558bbc73
zhujiajun <zhujiajun.me@gmail.com>
2016-09-22 16:06:33 +0800

    add log

--- src/main/java/com/github/zjiajun/adult/tool/LoggerTool.java
+++ src/main/java/com/github/zjiajun/adult/tool/LoggerTool.java
@@ -1,5 +1,8 @@
 package com.github.zjiajun.adult.tool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -10,6 +13,9 @@ import java.io.StringWriter;
  * 日志工具类
  */
 public final class LoggerTool {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(LoggerTool.class);
+    public static final Logger ERROR = LoggerFactory.getLogger("adult.error");
 
     private LoggerTool() {}
 
