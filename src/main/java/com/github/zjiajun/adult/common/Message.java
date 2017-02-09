9046de84de66d695ce97da6d14437b395a565930
zhujiajun <zhujiajun.me@gmail.com>
2017-02-09 22:03:25 +0800

    message

--- src/main/java/com/github/zjiajun/adult/common/Message.java
+++ src/main/java/com/github/zjiajun/adult/common/Message.java
@@ -6,6 +6,9 @@ package com.github.zjiajun.adult.common;
  */
 public final class Message<T> {
 
+    /**
+     * 消息类型
+     */
     private MessageType type;
 
     /**
@@ -13,11 +16,20 @@ public final class Message<T> {
      */
     private T data;
 
+    /**
+     * 时间戳
+     */
+    private long timestamp;
 
     public Message(MessageType type, T data) {
         this.type = type;
         this.data = data;
+        this.timestamp = System.currentTimeMillis();
     }
 
-
+    public Message(MessageType type, T data, long timestamp) {
+        this.type = type;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
