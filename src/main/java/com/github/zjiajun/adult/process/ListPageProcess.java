2d312f54cd19b36c0daf595309873544bcffbfff
zhujiajun <zhujiajun.me@gmail.com>
2017-02-14 21:02:59 +0800

    process

--- src/main/java/com/github/zjiajun/adult/process/ListPageProcess.java
+++ src/main/java/com/github/zjiajun/adult/process/ListPageProcess.java
@@ -3,6 +3,10 @@ package com.github.zjiajun.adult.process;
 import com.github.zjiajun.adult.Page;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author zhujiajun
@@ -14,7 +18,15 @@ public class ListPageProcess implements Process {
 
     @Override
     public void process(Page page) {
-        Document document = Jsoup.parse(page.getHtml());
+        Document listPageDoc = Jsoup.parse(page.getHtml());
+        Elements elements = listPageDoc.select("table[id^=forum]:contains(推荐主题) span a");
+        Map<String, String> urlMap = new LinkedHashMap<>();
+        elements.forEach(e -> {
+            String text = e.text();//a标签内容,即title
+            String href = e.absUrl("href");//详情url
+            String filePath = text.replaceAll("/", "");//替换斜杠，防止新建多层目录
+            urlMap.put(filePath, href);
+        });
 
     }
 
