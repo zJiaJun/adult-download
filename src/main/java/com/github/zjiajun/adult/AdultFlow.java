ccd1fb9db2c4e774c45476576e4eb28d806ec661
zhujiajun <zhujiajun.me@gmail.com>
2017-02-16 21:24:52 +0800

    flow

---
+++ src/main/java/com/github/zjiajun/adult/AdultFlow.java
@@ -0,0 +1,15 @@
+package com.github.zjiajun.adult;
+
+import com.github.zjiajun.adult.tool.AdultConfig;
+
+/**
+ * @author zhujiajun
+ * @since 2017/2/16
+ */
+public class AdultFlow {
+
+
+    public void flow(AdultConfig adultConfig) {
+        //流程处理
+    }
+}
