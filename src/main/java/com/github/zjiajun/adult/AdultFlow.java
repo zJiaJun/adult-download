cca1261470da605022e54322de19409095f54a41
zhujiajun <zhujiajun.me@gmail.com>
2017-02-17 21:43:36 +0800

    flow

--- src/main/java/com/github/zjiajun/adult/AdultFlow.java
+++ src/main/java/com/github/zjiajun/adult/AdultFlow.java
@@ -1,6 +1,11 @@
 package com.github.zjiajun.adult;
 
+import com.github.zjiajun.adult.input.Input;
+import com.github.zjiajun.adult.input.InputService;
 import com.github.zjiajun.adult.tool.AdultConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author zhujiajun
@@ -11,5 +16,14 @@ public class AdultFlow {
 
     public void flow(AdultConfig adultConfig) {
         //流程处理
+        List<Request> requestList = new ArrayList<>();
+        boolean needLogin = true;
+        if (needLogin) {
+            Request loginRequest = requestList.get(0);
+
+        }
+        Request request = new Request.Builder().build();
+        Input input = new InputService();
+        input.input(request);
     }
 }
