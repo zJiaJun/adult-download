c2ad8d401eaba075b55cbe75918f7a88203d3c0e
zhujiajun <zhujiajun.me@gmail.com>
2017-02-27 21:31:43 +0800

    output

---
+++ src/main/java/com/github/zjiajun/adult/output/FileOutput.java
@@ -0,0 +1,16 @@
+package com.github.zjiajun.adult.output;
+
+import com.github.zjiajun.adult.PageResult;
+
+/**
+ * @author zhujiajun
+ * @since 2017/2/27
+ */
+public class FileOutput implements Output {
+
+    @Override
+    public void output(PageResult result) {
+
+    }
+
+}
