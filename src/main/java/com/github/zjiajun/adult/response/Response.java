ce43333ecb117976e3a76b54659b80c673058916
zhujiajun <zhujiajun.me@gmail.com>
2017-04-10 22:09:37 +0800

    modify

---
+++ src/main/java/com/github/zjiajun/adult/response/Response.java
@@ -0,0 +1,12 @@
+package com.github.zjiajun.adult.response;
+
+/**
+ * @author zhujiajun
+ * @since 2017/4/10
+ */
+public class Response {
+
+    private String content;
+
+    private int statusCode;
+}
