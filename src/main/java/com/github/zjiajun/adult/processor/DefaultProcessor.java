048d61e6ceea20fbc0888b5be5609afcf54a1223
zhujiajun <zhujiajun.me@gmail.com>
2017-04-21 22:22:33 +0800

    processor

--- src/main/java/com/github/zjiajun/adult/processor/DefaultProcessor.java
+++ src/main/java/com/github/zjiajun/adult/processor/DefaultProcessor.java
@@ -3,6 +3,8 @@ package com.github.zjiajun.adult.processor;
 import com.github.zjiajun.adult.response.Response;
 import com.github.zjiajun.adult.scheduler.Scheduler;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author zhujiajun
  * @since 2017/4/19
@@ -19,6 +21,15 @@ public class DefaultProcessor implements Processor {
     public void process() {
         while (true) {
             Response response = scheduler.takeResponse();
+
+            String content = response.getContent();
+
+
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
 
