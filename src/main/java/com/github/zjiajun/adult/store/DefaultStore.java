367c82575dabf10ab4e5d8167e7f9754c928ce08
zhujiajun <zhujiajun.me@gmail.com>
2017-06-20 21:05:33 +0800

    modify

---
+++ src/main/java/com/github/zjiajun/adult/store/DefaultStore.java
@@ -0,0 +1,15 @@
+package com.github.zjiajun.adult.store;
+
+/**
+ * @author zhujiajun
+ * @since 2017/6/20
+ */
+public class DefaultStore implements Store {
+
+    @Override
+    public void store(Object obj) {
+
+    }
+
+
+}
