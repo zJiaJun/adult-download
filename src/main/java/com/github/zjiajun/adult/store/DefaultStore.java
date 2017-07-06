4fa52189587c2f071d2f47e2b13bbcbdf2c0ca55
zhujiajun <zhujiajun.me@gmail.com>
2017-07-06 21:25:19 +0800

    download

--- src/main/java/com/github/zjiajun/adult/store/DefaultStore.java
+++ src/main/java/com/github/zjiajun/adult/store/DefaultStore.java
@@ -1,7 +1,10 @@
 package com.github.zjiajun.adult.store;
 
 import com.github.zjiajun.adult.downloader.RetrofitClient;
+import com.github.zjiajun.adult.request.Request;
+import com.github.zjiajun.adult.response.Response;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -20,10 +23,16 @@ public class DefaultStore implements Store {
     public void store(StoreResult storeResult) {
         Map<String, String> resultMap = storeResult.getResultMap();
         if (null != resultMap && !resultMap.isEmpty()) {
-            resultMap.forEach((k, v) -> {
-                
-
-            });
+            try {
+                for (Map.Entry<String, String> entry : resultMap.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    Request downloadRequest = new Request.Builder().url(value).build();
+                    Response execute = retrofitClient.execute(downloadRequest);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }
     }
